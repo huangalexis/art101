22523e586d5c0aa0fe9fc46b33a4d72731c47b3a
huangalexis <95466160+huangalexis@users.noreply.github.com>
2023-05-03 23:27:26 -0700

    lab7

---
+++ lab7/js/lab.js
@@ -0,0 +1,25 @@
+// Author: Alexis Huang and Scott Kuwahara
+// Date: 3 May
+
+
+function sortUsername() {
+    var userName = window.prompt("GIVE ME YOUR NAME OR ELSE.");
+    console.log("userName = ", userName);
+   
+    var nameArray = userName.split('');
+    console.log("nameArray =", nameArray);
+ 
+ 
+    var nameArraySort = nameArray.sort();
+    console.log("nameArraySort=", nameArraySort);
+ 
+ 
+    var nameSorted = nameArraySort.join('');
+    console.log("nameSorted =", nameSorted);
+ 
+ 
+    return nameSorted;
+ }
+ 
+ 
+ document.writeln("I fixed your name: ", sortUsername(), "</br>");
